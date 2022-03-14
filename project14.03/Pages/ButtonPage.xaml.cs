065f2e1b8ff6c6cf4e34ddaeae558cfe50cabff0
dianavolfman <dianavolfman0@gmail.com>
2022-03-14 10:44:45 +0300

    qq
    qq

---
+++ project14.03/Pages/ButtonPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project14._03.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ButtonPage.xaml
+    /// </summary>
+    public partial class ButtonPage : Page
+    {
+        public ButtonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = Window.GetWindow(this) as MainWindow;
+            window.CenterFrame.Navigate(new Pages.LinearEquationPage());
+        }
+    }
+}
---
+++ project14.03/Pages/LinearEquationPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project14._03.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LinearEquationPage.xaml
+    /// </summary>
+    public partial class LinearEquationPage : Page
+    {
+        public LinearEquationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int k = Convert.ToInt32(kTextBox.Text);
+            int b = Convert.ToInt32(bTextBox.Text);
+            int itog = (k / b);
+            TextBox.Text = itog.ToString();
+
+        }
+    }
+}
